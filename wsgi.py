4b8022575a96716b143c40ff82f6c7950412b9b0
Joel Friedly <joelfriedly@gmail.com>
2011-06-18 08:53:04 -0400

    using jfriedly.settings now

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 import sys
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'jfriedly.settings'
 import django.core.handlers.wsgi
 djangoapplication = django.core.handlers.wsgi.WSGIHandler()
 def application(environ, start_response):
